d9e9de033800aa64af1fb95c899b7dddab4b002d
Seunghyeon Lee <seunghyeon@kaist.ac.kr>
2018-02-22 22:11:33 +0900

    Update ReviewCommand.java

--- cli/src/main/java/org/onosproject/cli/security/ReviewCommand.java
+++ cli/src/main/java/org/onosproject/cli/security/ReviewCommand.java
@@ -45,6 +45,7 @@ public class ReviewCommand extends AbstractShellCommand {
             required = false, multiValued = false)
     String accept = null;
 
+
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
